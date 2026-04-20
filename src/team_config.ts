07871e16bf9f2650ce482631f54c28d73f65250b
Joonas K. Luukkonen <joonas.k.luukkonen@gmail.com>
2026-04-20 16:03:34 +0300

    Update team_config.ts

--- src/team_config.ts
+++ src/team_config.ts
@@ -1,6 +1,6 @@
 export const TEAM_ASSIGNMENTS = {
   "Asia": {
-    assignee: "JOONAS L.",
+    assignee: "JOONAS L. & HANIE",
     color: [[0, 242, 255], // RGB format [R, G, B]
     centroid: [90.0, 45.0], // [Longitude, Latitude]
     radius: 5000000 // Radar radius in meters
@@ -24,14 +24,14 @@ export const TEAM_ASSIGNMENTS = {
     radius: 2500000
   },
   "Africa": {
-    assignee: "Topias Remes",
-    color: [255, 170, 0],
+    assignee: "UNASSIGNED",
+    color: [80, 80, 80],
     centroid: [20.0, 5.0],
     radius: 4500000
   },
   "Oceania": {
-    assignee: "UNASSIGNED",
-    color: [80, 80, 80],
+    assignee: "Topias Remes",
+    color: [255, 170, 0],
     centroid: [135.0, -25.0],
     radius: 3500000
   }
